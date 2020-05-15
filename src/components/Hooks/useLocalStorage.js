8a36223fb45c22ebca544206a22b192a4c978114
Andre <andrecolon@gmail.com>
2020-05-14 19:13:31 -0700

    MVP done

--- src/components/Hooks/useLocalStorage.js
+++ src/components/Hooks/useLocalStorage.js
@@ -1,17 +1,26 @@
-import React, {useState} from 'react'
+import { useState } from "react";
 
+// To retrieve an item from localStorage, call localStorage.getItem('itemName')
+// If that item doesn't exist, it will return undefined
+ const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        if(window.localStorage.getItem(key)){
+            // Get from local storage by key
+            return JSON.parse(window.localStorage.getItem(key));
+            // Parse and return stored json or, if undefined, return initialValue
+        }
+     window.localStorage.setItem(key, JSON.stringify(initialValue))
+     return initialValue
+    });          
+    const setValue = value => {
+        // Save state
+        setStoredValue(value);
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
 
-
-export const useLocalStorage =() => {
-    const [value, setValue] = useState(() =>{
-        if(window.localStoraga.getitem(key)) {
-            return JSON.parse(window.localStorage.getItem(key))
-    )}
-        return initialValue
-    }
-    return (
-        <div>
-            Hello World
-        </div>
-    )
+    return [storedValue, setValue]
+    
 }
+
+export default useLocalStorage
